4bae89f2fa33cf5743b1311615ec1daffc3c7189
Pedro Gustavo Santana <pedro.parametrize@gmail.com>
2025-07-31 20:57:23 -0300

    fix: fix up container

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files (User uploaded files)
 MEDIA_URL = "/media/"
